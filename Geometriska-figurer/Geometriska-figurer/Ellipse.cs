160fc3b83f9397732666d8b1f33d61b63f60efad
jt222ii <jt222ii@student.lnu.se>
2014-10-09 15:03:32 +0200

    initial commit and started some coding

---
+++ Geometriska-figurer/Geometriska-figurer/Ellipse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geometriska_figurer
+{
+    class Ellipse : Shape
+    {
+        public double Area { get; }
+        public double Perimeter { get; }
+
+        public Ellipse(double length, double width): base(length, width)
+        {
+
+        }
+    }
+}
---
+++ Geometriska-figurer/Geometriska-figurer/Shape.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geometriska_figurer
+{
+    abstract class Shape
+    {
+        private double _length;
+        private double _width;
+        public abstract double Area
+        {
+            get;
+        }
+        public double Length
+        {
+            
+        }
+        public abstract double Perimeter
+        {
+            get;
+        }
+        protected Shape(double length, double width)
+        {
+            Length = length;
+            Width = width;
+        }
+        public double Width
+        {
+
+        }
+        public override string ToString()
+        {
+
+        }
+    }
+}
